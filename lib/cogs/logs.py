54e7f352d07242bf14a3fab4b76ca1cc3b2dbd0c
dapinaple <69084408+dapinaple@users.noreply.github.com>
2021-02-14 16:14:31 -0500

    Update logs.py
    defining the self rather than the bot

--- lib/cogs/logs.py
+++ lib/cogs/logs.py
@@ -10,7 +10,7 @@ import asyncio
 
 class Logs(Cog):
     def __init__(self, bot):
-        self.client = bot
+        self.bot = bot
         self.listOfClipees =  [673403025410359337,]    
         self.estelogs = self.bot.get_channel(809099936720617548)
         self.ghostLogs = self.bot.get_channel(809099936180338742)
@@ -32,22 +32,21 @@ class Logs(Cog):
             if message.content.lower() == "fuck you poopshitter":
                 await message.channel.send('''***Ah nigga don't hate me cause I'm beautiful nigga. Maybe if you got rid of that old yee yee ass haircut, you'd get some bitches on yo dick.
                                                 Oh, better yet, maybe Tanisha'll call your dog ass if she stops fuckin' with that brain surgeon or lawyer she fucking with. Niiggaaa***''')
-                print("this came from logs cog")
                 break
-        if message.author.id in listOfClipees:
+        if message.author.id in self.listOfClipees:
             if message.content.startswith("https") or message.content.startswith("cdn") or len(message.attachments) != 0 and message.channel.id != 784152789042593832:
                 for attachment in message.attachments:
                     await self.estelogs.send(f"{message.author} sent this: {attachment.url} in {message.channel.mention} in the server **{message.guild}**")
                 if message.content.startswith("https") or message.content.startswith("cdn"):
                     await self.estelogs.send(f"{message.author} sent this {message.content} in {message.channel.mention} in the server **{message.guild}**")
             
-        if isinstance(message.channel, channel.DMChannel) and message.author != client.user:
+        if isinstance(message.channel, channel.DMChannel) and message.author != bot.user:
             await message.channel.send("https://tenor.com/view/dono-wall-talking-wall-bricks-gif-17741481")
             # elif word.lower() in forbiddenWords:
             #     await message.author.send("nword bad")
             #     print(f"send nword response to {message.author.name}")  
 
-        await client.process_commands(message)
+        await self.bot.process_commands(message)
     @Cog.listener()
     async def on_message_delete(self,message):
         if not message.author.bot:
@@ -56,7 +55,7 @@ class Logs(Cog):
                 embed = Embed(title = 'Ghost Ping Found!',description = '', color = Color.dark_blue())
                 embed.add_field(name = "User:",value = message.author,inline = True)
                 embed.add_field(name = "Message:",value = message.content,inline = True)
-                embed.set_thumbnail(url = client.user.avatar_url)
+                embed.set_thumbnail(url = bot.user.avatar_url)
 
                 d = datetime.now()
                 timezone = pytz.timezone("America/New_York")
@@ -67,7 +66,7 @@ class Logs(Cog):
                 await ghostLogs.send(embed = embed)
             
             #clipping ppl i.e. estebanlol
-            if message.author.id in listOfClipees and not message.content.startswith("https") : 
+            if message.author.id in self.listOfClipees and not message.content.startswith("https") : 
                 embed =Embed(title = '',description = f'**{message.author.mention} sent a message in {message.channel.mention} in {message.guild}**\n {message.content}',color = Color.dark_blue())
                 embed.set_author(name = message.author,icon_url = message.author.avatar_url)
                     
