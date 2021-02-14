b278f2adb676daec0dab653b52b86b9c4dc5a90d
dapinaple <69084408+dapinaple@users.noreply.github.com>
2021-02-14 12:50:09 -0500

    Update logs.py
    added on msg delete listener

--- lib/cogs/logs.py
+++ lib/cogs/logs.py
@@ -1,22 +1,29 @@
 from discord import Member, guild
-from discord.ext import commands
-from discord.ext.commands import BucketType, CommandOnCooldown, cooldown
-from discord.permissions import Permissions
-from discord.utils import get
-from discord.voice_client import VoiceClient
-from discord import channel
+from discord.ext.commands import Cog
+from datetime import datetime
+from discord.ext.commands import command
+from discord import Embed
+import pytz
+from discord import channel, Color
 import asyncio
 
 
-class Logs(commands.Cog):
+class Logs(Cog):
     def __init__(self, bot):
         self.client = bot
         self.listOfClipees =  [673403025410359337,]    
-                
+        self.estelogs = self.bot.get_channel(809099936720617548)
+        self.ghostLogs = self.bot.get_channel(809099936180338742)
 
-    @listener()
+    @Cog.listener()
+    async def on_ready(self):
+        if not self.bot.ready:
+            self.logChannel = self.bot.get_channel(808408001823113237)
+            self.bot.cogs_ready.ready_up("log")
+            
+
+    @Cog.listener()
     async def on_message(self,message):
-        logs = client.get_channel(809099936720617548)
         
         #nobody disses my bot
 
@@ -30,9 +37,9 @@ class Logs(commands.Cog):
         if message.author.id in listOfClipees:
             if message.content.startswith("https") or message.content.startswith("cdn") or len(message.attachments) != 0 and message.channel.id != 784152789042593832:
                 for attachment in message.attachments:
-                    await logs.send(f"{message.author} sent this: {attachment.url} in {message.channel.mention} in the server **{message.guild}**")
+                    await self.estelogs.send(f"{message.author} sent this: {attachment.url} in {message.channel.mention} in the server **{message.guild}**")
                 if message.content.startswith("https") or message.content.startswith("cdn"):
-                    await logs.send(f"{message.author} sent this {message.content} in {message.channel.mention} in the server **{message.guild}**")
+                    await self.estelogs.send(f"{message.author} sent this {message.content} in {message.channel.mention} in the server **{message.guild}**")
             
         if isinstance(message.channel, channel.DMChannel) and message.author != client.user:
             await message.channel.send("https://tenor.com/view/dono-wall-talking-wall-bricks-gif-17741481")
@@ -41,3 +48,30 @@ class Logs(commands.Cog):
             #     print(f"send nword response to {message.author.name}")  
 
         await client.process_commands(message)
+    @Cog.listener()
+    async def on_message_delete(self,message):
+        if not message.author.bot:
+           
+            if len(message.mentions) > 0 and message.guild.id == 809099935663652954:
+                embed = Embed(title = 'Ghost Ping Found!',description = '', color = Color.dark_blue())
+                embed.add_field(name = "User:",value = message.author,inline = True)
+                embed.add_field(name = "Message:",value = message.content,inline = True)
+                embed.set_thumbnail(url = client.user.avatar_url)
+
+                d = datetime.now()
+                timezone = pytz.timezone("America/New_York")
+                d_aware = timezone.localize(d)
+                
+                embed.timestamp = d_aware
+                
+                await ghostLogs.send(embed = embed)
+            
+            #clipping ppl i.e. estebanlol
+            if message.author.id in listOfClipees and not message.content.startswith("https") : 
+                embed =Embed(title = '',description = f'**{message.author.mention} sent a message in {message.channel.mention} in {message.guild}**\n {message.content}',color = Color.dark_blue())
+                embed.set_author(name = message.author,icon_url = message.author.avatar_url)
+                    
+                await estelogs.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Logs(bot))
