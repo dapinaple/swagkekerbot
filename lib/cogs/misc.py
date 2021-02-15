c0ad4153e9c86d65f89d014edddd6c123d3b9d20
dapinaple <69084408+dapinaple@users.noreply.github.com>
2021-02-14 20:04:47 -0500

    change name of commands.py to misc

--- lib/cogs/misc.py
+++ lib/cogs/misc.py
@@ -9,12 +9,13 @@ class Commands(Cog):
     
     async def on_ready(self):
         if not self.bot.ready:
-            self.bot.cogs_ready.ready_up("commands")
+            self.bot.cogs_ready.ready_up("misc")
 
 
     @has_permissions(manage_messages = True)
-    @command(name = "clear")
+    @command(name = "clear",brief = "Clears the chat of 10 or <amount> messages")
     async def clear(self,ctx,amount = 10):
+        
         if ctx.author.id in self.bannedMembers:  
             await ctx.send("stfu")
         else:
@@ -27,8 +28,17 @@ class Commands(Cog):
                 await ctx.channel.send("***{}***".format(choice(list_of_responses)))
 
                 
-    @command(name = "ping",brief = "")
+    @command(name = "ping",brief = "Gets the connection of something idk")
     async def ping(self,ctx):
         await ctx.send(f"**PONG** {round(self.bot.latency * 1000)}ms")
+   
+    @command(name = "dav1", brief = "Checks if you are the infamous Dav1")
+    async def dav1(self,ctx):
+        if ctx.author.id == 438809594291027969:
+            await ctx.send("***OH GOD ITS YOU MOTHERFUCKING BITCH***")
+        else:
+            await ctx.send(":pensive:***no...you're not him***")
+            
+            
 def setup(bot):
     bot.add_cog(Commands(bot))
