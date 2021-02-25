a110fe575d172d87486782ea14b78517ed61276b
dapinaple <69084408+dapinaple@users.noreply.github.com>
2021-02-25 17:43:08 -0500

    Update help.py
    now hidden commands are a thing

--- lib/cogs/help.py
+++ lib/cogs/help.py
@@ -19,27 +19,48 @@ def syntax(command):
 class HelpMenu(ListPageSource):
     def __init__(self,ctx,data):
         self.ctx = ctx
+        self.entries_to_remove = len([i for i in data if i.hidden == True])
+        
+        per_page = 3
+        
+        
+        
 
         super().__init__(data,per_page=3)
+        
+
+        if self.ctx.author.id == 426549783864279040:
+            self.hidden_items = per_page -1
+        else:
+            self.hidden_items = per_page -len([i for i in data if i.hidden == True]) 
+
     async def write_page(self,menu,fields=[]):
         offset = (menu.current_page*self.per_page)+1
-        len_data = len(self.entries)
+        len_data = len(self.entries)-self.entries_to_remove
+        
         
         embed = Embed(title = "Help",
                      description = "Poopshitter help dialog",
                      color = Color.dark_blue())
         embed.set_thumbnail(url=self.ctx.guild.me.avatar_url)
-        embed.set_footer(text=f"{offset:,} - {min(len_data, offset+self.per_page-1):,} of {len_data:,} commands.")
+        embed.set_footer(text=f"{offset:,} - {min(len_data, offset+self.hidden_items):,} of {len_data:,} commands.")
 
         for name,value in fields:
             embed.add_field(name = f"poo.{value}", value = name, inline = False)
+        
+            
 
         return embed
     async def format_page(self, menu, entries):
         fields = []
 
         for entry in entries:
-            fields.append((entry.brief or "No description", syntax(entry)))
+            if entry.hidden == False or self.ctx.author.id == 426549783864279040:
+                fields.append((entry.brief or "No description", syntax(entry)))
+            else:
+                
+                continue
+                
         return await self.write_page(menu,fields)
 
 
@@ -57,9 +78,9 @@ class Help(Cog):
         embed.add_field(name = "Command description:",value = command.brief)
         await ctx.send(embed = embed)
 
-    @command(name = "help",brief = "Shows this message", hidden = True)
+    @command(name = "help",brief = "Shows this message")
     async def show_help(self,ctx,cmd:Optional[str]):
-        self.show_hidden = True if ctx.guild.id == 809099935663652954 else False
+        self.show_hidden = False
         if cmd is None:
             menu = MenuPages(source = HelpMenu(ctx,list(self.bot.commands)),
                             delete_message_after = True,
