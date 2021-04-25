4b3549cb2df7e1b64388f96c8ce54f42f733110d
dapinaple <69084408+dapinaple@users.noreply.github.com>
2021-04-24 20:36:58 -0400

    added modusage table in db

--- data/db/build.sql
+++ data/db/build.sql
@@ -2,3 +2,8 @@ CREATE TABLE IF NOT EXISTS guilds (
         GuildID integer PRIMARY KEY,
         Prefix text DEFAULT "poo."
 );
+
+CREATE TABLE IF NOT EXISTS modusage (
+        daysSinceFunny integer,
+        dateOfFunny date
+);
