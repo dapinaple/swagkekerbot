4a3767ae6d50121420094c78c7110458f55483ba
dapinaple <69084408+dapinaple@users.noreply.github.com>
2021-04-24 20:37:03 -0400

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,9 +1,22 @@
 from lib.bot import bot
 
-
 def main():
     bot.run()
 
 if __name__ == "__main__":
-    main()
-    
+    main()  
+
+
+
+"""
+chief's  will
+
+GIVE POOPSHITTER CODE TO SANTI
+
+GIVE PC TO DAV
+GIVE CLOTHES TO ESTEBAN C
+GIVE GUITARS TO ALEX
+
+GIVE TEA TO DEVON
+
+"""
